b34cf94b4868eb4ac76dede9a82dbbe7a98b4d59
swahpy <hitwhy@gmail.com>
2024-12-17 09:39:48 +0000

    add lsp config of basedpyright

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,11 +1,11 @@
 -- load defaults i.e lua_lsp
-require("nvchad.configs.lspconfig").defaults()
+local nvlsp = require "nvchad.configs.lspconfig"
+nvlsp.defaults()
 
 local lspconfig = require "lspconfig"
 
 -- EXAMPLE
-local servers = { "html", "cssls" }
-local nvlsp = require "nvchad.configs.lspconfig"
+local servers = { "html", "cssls", "basedpyright" }
 
 -- lsps with default config
 for _, lsp in ipairs(servers) do
@@ -22,3 +22,30 @@ end
 --   on_init = nvlsp.on_init,
 --   capabilities = nvlsp.capabilities,
 -- }
+lspconfig.basedpyright.setup {
+  on_attach = nvlsp.on_attach,
+  on_init = nvlsp.on_init,
+  capabilities = nvlsp.capabilities,
+  settings = {
+    basedpyright = {
+      disableOrganizeImports = true,
+      analysis = {
+        autoImportCompletions = true,
+        autoSearchPaths = true,
+        useLibraryCodeForTypes = true,
+        diagnosticsMode = "openFilesOnly", -- workspace, openFilesOnly
+        typeCheckingMode = "standard", -- off, basic, standard, strict, all
+        diagnosticSeverityOverrides = {
+          reportUnknownMemberType = false,
+          reportUnknownArgumentType = false,
+          reportUnusedVariable = false, -- ruff handles this with F841
+          reportUnusedImport = false, -- ruff handles this with F401
+          reportAttributeAccessIssue = false,
+        },
+      },
+    },
+    python = {
+      pythonPath = "./venv/bin/python",
+    },
+  },
+}
