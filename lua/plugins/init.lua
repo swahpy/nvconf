3ad337330bf47b553affbc2ad3f790a66e86693e
swahpy <hitwhy@gmail.com>
2024-12-29 21:52:42 +0800

    add plugin folke/flash.nvim

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -64,4 +64,30 @@ return {
     event = "InsertCharPre",
     config = true,
   },
+
+  {
+    "folke/flash.nvim",
+    event = "VeryLazy",
+    opts = {
+      modes = {
+        -- f, t, F, T with labels
+        char = {
+          jump_labels = true,
+        },
+        search = {
+          -- when `true`, flash will be activated during regular search by default.
+          -- You can always toggle when searching with `require("flash").toggle()`
+          enabled = true,
+        },
+      },
+    },
+    -- stylua: ignore
+    keys = {
+      { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+      { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+      { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+      { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+      { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+    },
+  },
 }
