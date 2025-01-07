bc0d555a2375c63ac28a8261f891bc7ac8f9d0c1
swahpy <hitwhy@gmail.com>
2025-01-07 17:21:08 +0800

    add plugin lspsaga

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -151,4 +151,16 @@ return {
       { "<leader>ut", "<cmd>UndotreeToggle<cr>", desc = "Toggle undo tree" },
     },
   },
+
+  {
+    "nvimdev/lspsaga.nvim",
+    event = "LspAttach",
+    config = function()
+      require("lspsaga").setup {}
+    end,
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter", -- optional
+      "nvim-tree/nvim-web-devicons", -- optional
+    },
+  },
 }
