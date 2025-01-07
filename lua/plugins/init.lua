12e883d3a9e2b56b5b49444d901347312d0f67e3
swahpy <hitwhy@gmail.com>
2025-01-07 17:51:57 +0800

    add plugin arrow

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -163,4 +163,21 @@ return {
       "nvim-tree/nvim-web-devicons", -- optional
     },
   },
+
+  {
+    "otavioschwanck/arrow.nvim",
+    dependencies = {
+      { "nvim-tree/nvim-web-devicons" },
+      -- or if using `mini.icons`
+      -- { "echasnovski/mini.icons" },
+    },
+    opts = {
+      show_icons = true,
+      leader_key = "-", -- Recommended to be a single key
+      buffer_leader_key = "m", -- Per Buffer Mappings
+    },
+    keys = {
+      {"-"}
+    }
+  },
 }
