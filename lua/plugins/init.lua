fa0e5ba96c0ef84a81d9e8f80474549c786e4602
swahpy <hitwhy@gmail.com>
2025-01-08 17:14:18 +0800

    add CodeEdit functionality to markview codeblocks

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -125,6 +125,30 @@ return {
       "nvim-tree/nvim-web-devicons",
     },
     opts = require "configs.markview",
+    config = function(_, opts)
+      require("markview").setup(opts)
+      require("markview.extras.editor").setup {
+        --- The minimum & maximum window width
+        --- If the value is smaller than 1 then
+        --- it is used as a % value.
+        ---@type [ number, number ]
+        width = { 10, 0.75 },
+
+        --- The minimum & maximum window height
+        ---@type [ number, number ]
+        height = { 3, 0.75 },
+
+        --- Delay(in ms) for window resizing
+        --- when typing.
+        ---@type integer
+        debounce = 50,
+
+        --- Callback function to run on
+        --- the floating window.
+        ---@type fun(buf:integer, win:integer): nil
+        callback = function(buf, win) end,
+      }
+    end,
   },
 
   {
